a95bc55bee0d591c9b0616ee743b69414a38ed07
Vishnu Priya Singam reddy <vishnupriyasingamreddy6@gmail.com>
2026-08-25 16:07:25 +0530

    Time: 5 ms (62.8%), Space: 82.9 MB (59.24%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,11 +1,23 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==nums[i+1])
-                return nums[i];
-        }    
-        return -1;
-        
+
+        int slow = nums[0];
+        int fast = nums[0];
+
+        // Step 1: Find intersection point
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        // Step 2: Find entrance of cycle
+        slow = nums[0];
+
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
     }
 }
