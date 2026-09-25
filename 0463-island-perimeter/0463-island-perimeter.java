cf9d9160006e5b4cf804d9bc9703980119bd5e8d
Vishnu Priya Singam reddy <vishnupriyasingamreddy6@gmail.com>
2026-09-25 15:59:16 +0530

    Time: 7 ms (19.77%), Space: 47.1 MB (82.68%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.java
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -1,25 +1,28 @@
 class Solution {
-    private int dfs(int[][] grid,int r, int c){
-        //out of bounds condition along with wter
-        if(r<0||c<0||r>=grid.length||
-        c>=grid[0].length||grid[r][c]==0) return 1;
-        //land=no perimeter
-        if(grid[r][c]==-1) return 0;
-        //check all the four sides and return the perimeter
-        grid[r][c] = -1;
-        return dfs(grid,r-1,c)+
-        dfs(grid,r+1,c)+
-        dfs(grid,r,c+1)+
-        dfs(grid,r,c-1);
+    private int dfs(int[][] grid,int r,int c){
+        //out of bounds cond along with water
+        if(r<0 || c<0 || r>= grid.length || 
+        c>=grid[0].length || grid[r][c]==0) return 1;
+       
+        if(grid[r][c]==2) return 0;
+        //visited cell marking 
+        grid[r][c]=2;
+        if(grid[r][c]==1) return 0;
+        
+        //check all the four sides and return the perim 
+        return dfs(grid, r-1, c)+
+        dfs(grid, r+1, c)+
+        dfs(grid, r, c-1)+
+        dfs(grid, r, c+1) ;
     }
     public int islandPerimeter(int[][] grid) {
-        int n=grid.length;
-        int m=grid[0].length;
-        int perim=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        int n = grid.length;
+        int m = grid[0].length;
+        int perim = 0;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
                 if(grid[i][j]==1){
-                    perim+=dfs(grid,i,j);
+                    perim += dfs(grid, i, j);
                 }
             }
         }
