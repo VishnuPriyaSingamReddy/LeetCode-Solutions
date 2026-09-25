44c6355b8f2993d89d2161104366f5cd3974748b
Vishnu Priya Singam reddy <vishnupriyasingamreddy6@gmail.com>
2026-09-25 18:27:10 +0530

    Time: 6 ms (99.61%), Space: 61.1 MB (29.84%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,50 +1,49 @@
 class Solution {
     private void dfs(int[][] grid, int r, int c) {
 
-        if (r < 0 || c < 0 || 
-            r >= grid.length || c >= grid[0].length ||
-            grid[r][c] == 0) {
+        // out of bounds condition
+        if(r < 0 || c < 0 || 
+           r >= grid.length || c >= grid[0].length ||
+           grid[r][c] == 0)
             return;
-        }
 
-        // Remove boundary-connected land
+        // visited cell marking with water
         grid[r][c] = 0;
 
-        dfs(grid, r - 1, c);
-        dfs(grid, r + 1, c);
-        dfs(grid, r, c - 1);
-        dfs(grid, r, c + 1);
+        dfs(grid, r-1, c);
+        dfs(grid, r+1, c);
+        dfs(grid, r, c-1);
+        dfs(grid, r, c+1);
     }
 
     public int numEnclaves(int[][] grid) {
-
         int n = grid.length;
         int m = grid[0].length;
 
-        // Remove land connected to top and bottom
-        for (int j = 0; j < m; j++) {
-            if (grid[0][j] == 1)
+        // check first and last row
+        for(int j = 0; j < m; j++){
+            if(grid[0][j] == 1)
                 dfs(grid, 0, j);
 
-            if (grid[n - 1][j] == 1)
-                dfs(grid, n - 1, j);
+            if(grid[n-1][j] == 1)
+                dfs(grid, n-1, j);
         }
 
-        // Remove land connected to left and right
-        for (int i = 0; i < n; i++) {
-            if (grid[i][0] == 1)
+        // check first and last columns
+        for(int i = 0; i < n; i++){
+            if(grid[i][0] == 1)
                 dfs(grid, i, 0);
 
-            if (grid[i][m - 1] == 1)
-                dfs(grid, i, m - 1);
+            if(grid[i][m-1] == 1)
+                dfs(grid, i, m-1);
         }
 
-        // Count remaining land
+        // count remaining land
         int count = 0;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 1) {
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(grid[i][j] == 1){
                     count++;
                 }
             }
